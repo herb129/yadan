bb54c9dbda5941af1eb9bc6815d9a473f0f15de4
yadan <527161751@qq.com>
2016-05-25 15:50:44 +0800

    新建的一个项目

---
+++ my_test/my_test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  my_test
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ my_test/my_testTests/my_testTests.m
@@ -0,0 +1,39 @@
+//
+//  my_testTests.m
+//  my_testTests
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface my_testTests : XCTestCase
+
+@end
+
+@implementation my_testTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
